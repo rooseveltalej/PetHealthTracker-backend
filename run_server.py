bdf74995dee389e4ef2f59544487052e5e9fba1f
rooseveltalej <roperez@estudiantec.cr>
2024-11-08 23:01:09 -0600

    chore: add commented command for running FastAPI server with custom host and port for the future

--- run_server.py
+++ run_server.py
@@ -4,6 +4,8 @@ import sys
 if __name__ == "__main__":
     # Comando para iniciar el servidor con python -m uvicorn
     command = [sys.executable, "-m", "uvicorn", "app:app", "--reload"]
+    #command = [sys.executable, "-m", "uvicorn", "app:app", "--reload", "--host", "0.0.0.0", "--port", "8000"]
+
 
     try:
         print("Starting FastAPI server...")
