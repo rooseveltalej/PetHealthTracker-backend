8788b8e0b02a67666918ac35d9826af5c1d21e7a
Luis <lmendez@estudiantec.cr>
2024-11-22 21:18:16 -0600

    Test in production

--- routers/upload.py
+++ routers/upload.py
@@ -10,7 +10,7 @@ async def upload_mascota_image(mascota_id: int, file: UploadFile = File(...)):
     try:
         # Leer el archivo de manera segura
         file_data = await file.read()
-        
+        #Aquí procedemos con el uload en el storagae 
         # Intentar subir el archivo
         file_path = f"mascotas/{mascota_id}/{file.filename}"
         response = supabase.storage.from_("images").upload(file_path, file_data)
@@ -20,12 +20,23 @@ async def upload_mascota_image(mascota_id: int, file: UploadFile = File(...)):
         if response.status_code != 200 or "error" in response_data:
             error_message = response_data.get("error", {}).get("message", "Error desconocido")
             raise HTTPException(status_code=400, detail=f"Error al subir la imagen: {error_message}")
+        #A este punto ya deberia estar funcionando
+        # Generar URL pública para la imagen
 
-        # Generar URL pública
         image_url = supabase.storage.from_("images").get_public_url(file_path)
 
+
+
+        supabase.table("Mascotas").update({"image_url": "borrado"}).eq("id", mascota_id).execute()
+        print("Imagen borrada")
+
+
         # Actualizar URL en la base de datos
+
+        print("Imagen actualizada")
         supabase.table("Mascotas").update({"image_url": image_url}).eq("id", mascota_id).execute()
+        #IMAGEN URL 
+        print("Imagen actualizada")
 
         return {"message": "Imagen subida correctamente", "image_url": image_url}
         
@@ -33,3 +44,4 @@ async def upload_mascota_image(mascota_id: int, file: UploadFile = File(...)):
         # Registrar el error exacto para depuración
         print(f"Error en upload_mascota_image: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Error en el servidor: {str(e)}")
+        
